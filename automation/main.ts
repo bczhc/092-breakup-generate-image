407589ab43eb8e1ba7bdaca64c6f54eb0198fb59
Zhai Can <bczhc0@126.com>
2023-12-16 17:30:28 +0800

    implement a socket server
    Now add two server types: socket, http

--- automation/main.ts
+++ automation/main.ts
@@ -1,8 +1,7 @@
 import * as puppeteer from 'puppeteer-core'
-import * as server from './server'
+import * as socketServer from './server/socket';
 import * as assert from "assert";
 import * as base64js from 'base64-js'
-import * as fs from 'fs'
 
 let HEADLESS = false
 let WEBPAGE_URL = 'http://localhost:8080'
@@ -24,12 +23,17 @@ async function loadPage(browser, url): Promise<puppeteer.Page> {
 
 let argv = process.argv.slice(2);
 if (argv.length === 0) {
-    console.log('Usage: command <webpage-url> <listen-port> [<headless-flag>]');
+    console.log('Usage: command <webpage-url> <listen-port> <server-type> [<headless>]');
+    console.log();
+    console.log('\tServer type: socket|http|all');
+    console.log('\tHeadless: yes|no (default: yes)')
     process.exit(1);
 }
 WEBPAGE_URL = argv[0];
 LISTEN_PORT = parseInt(argv[1])
-if (argv[2] != undefined) HEADLESS = true;
+let serverType = argv[2];
+let headless = argv[3];
+if (headless == undefined || headless == 'yes') HEADLESS = true;
 
 (async () => await main())();
 
@@ -37,18 +41,16 @@ async function main() {
     let browser = await launchBrowser();
     let page = await loadPage(browser, WEBPAGE_URL)
 
-    server.start(LISTEN_PORT, async c => {
+    socketServer.start(LISTEN_PORT, async text => {
         let data = await page.evaluate((text) => {
             let func = window['updateAndGetImage']
             console.log(func);
             return func(text) as string
-        }, c.text)
+        }, text)
 
         let lead = 'data:image/png;base64,';
         assert(data.startsWith(lead))
         let base64 = data.substring(lead.length);
-        let raw = base64js.toByteArray(base64);
-        console.log(`Write file: ${c.path}, length: ${raw.length}`)
-        fs.writeFileSync(c.path, raw)
+        return base64js.toByteArray(base64)
     })
 }
---
+++ automation/server/socket.ts
@@ -0,0 +1,61 @@
+import * as net from "net";
+import * as readline from "readline";
+
+type Input = string
+type Callback = (text: string) => Promise<Uint8Array>
+
+/**
+ * Start the server
+ *
+ * # Contract:
+ *
+ * - Receive: [JSON line (in UTF-8)]\n
+ * - Response: [Data Length (u32LE)][Image Data]
+ *
+ * JSON format: {@link Input}
+ *
+ * e.g. `"内容"`
+ *
+ * # Notes
+ * If the input content is invalid (e.g. not a String JSON), this line will
+ * be ignored silently
+ * @param port
+ * @param requestData
+ */
+export function start(port: number, requestData: Callback) {
+    const server = net.createServer((socket) => {
+        console.log(`Connected: ${socket}`)
+        const rl = readline.createInterface({
+            input: socket,
+        });
+
+        rl.on('line', async (line) => {
+            console.log(`on line: ${line}`);
+            try {
+                const input: Input = JSON.parse(line);
+                const imageData = await requestData(input);
+                const dataLengthBuffer = Buffer.alloc(4);
+                dataLengthBuffer.writeUInt32LE(imageData.length, 0);
+
+                // Sending [Data Length (u32)][Image Data]
+                socket.write(Buffer.concat([dataLengthBuffer, imageData]));
+            } catch (error) {
+                // Ignore invalid JSON lines
+                console.error(`Error processing line: ${error.message}`);
+            }
+        });
+
+        // Add 'end' event listener
+        socket.on('end', () => {
+            console.log('Client disconnected');
+        });
+    });
+
+    // Add 'error' event listener
+    server.on('error', (err) => {
+        console.error('Server error:', err.message);
+    });
+
+    server.listen(port, '0.0.0.0')
+    console.log(`Server started on port ${port}`)
+}
