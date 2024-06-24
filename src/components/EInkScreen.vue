11bf80fd8e6b2fd99c5fb2d43837f24c81328dd6
Zhai Can <bczhc0@126.com>
2024-06-25 03:18:27 +0800

    EInkScreen: use bitmap font

--- src/components/EInkScreen.vue
+++ src/components/EInkScreen.vue
@@ -17,21 +17,28 @@ let imageView = ref(null);
 registerSimpleImageGenerator(appName, async () => {
   return takeScreenshot(imageView.value, 1)
 })
+
+interface Params {
+  electricity: number,
+  toZeroDate: string,
+}
+
 </script>
 
 <template>
   <div id="border-wrapper">
     <div id="main-screen" ref="imageView">
-      <span style="font-family: 'Noto Sans CJK SC', sans-serif" id="line1" class="text-line">测试</span><br>
-      <span style="font-family: 'Noto Sans CJK SC', sans-serif" id="line2" class="text-line">₿: 64000$</span>
+      <span id="line1" class="text-line">宿舍电量：{{ (props.params as Params).electricity }} kW·h</span><br>
+      <span id="line2" class="text-line">归零时间：{{ (props.params as Params).toZeroDate }}</span><br>
+      <span is="line3" class="text-line">今日天气：TODO</span>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 #main-screen {
-  width: 250px;
-  height: 122px;
+  width: 212px;
+  height: 104px;
 }
 
 #border-wrapper {
@@ -39,18 +46,25 @@ registerSimpleImageGenerator(appName, async () => {
   border: black solid 1px;
 }
 
+// noinspection CssInvalidPropertyValue, CssUnknownProperty
 .text-line {
-  font-size: 25px;
+  font-smooth: never;
+  -moz-osx-font-smoothing: never;
+  -webkit-font-smoothing: never;
+  font-size: 16px;
   margin-left: 10px;
   display: inline;
-}
-
-#line2 {
-  padding-top: 0;
+  line-height: 1;
+  font-family: 'unifont', sans-serif;
 }
 
 @font-face {
   font-family: 'Noto Sans';
   src: url('/NotoSans-Regular.ttf');
+}
+
+@font-face {
+  font-family: 'unifont';
+  src: url('/unifont-15.1.05.otf')
 }
 </style>
--- src/params-data.ts
+++ src/params-data.ts
@@ -8,5 +8,7 @@ export let appParams = ref({
         text: '',
     },
     'eink-screen': {
+        electricity: 123.45,
+        toZeroDate: '05-09 12:34'
     }
 })
